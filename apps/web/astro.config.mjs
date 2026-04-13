e9e847f5acf26645cb5393eea0138ce254a44461
MapIhs <maptuhul2011@gmail.com>
2026-04-13 16:27:54 +0700

    fix lint

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+
+import svelte from "@astrojs/svelte";
 
 import tailwindcss from "@tailwindcss/vite";
-import svelte from "@astrojs/svelte";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
